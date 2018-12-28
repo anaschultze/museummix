ecdef686f06bd6e04fae815a766bc2c89a80fca9
anaschultze <acmschultze@gmail.com>
2018-12-28 17:23:35 +0100

    rijksmuseum images loading

--- functions.js
+++ functions.js
@@ -1,57 +1,35 @@
-//credits to https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_trigger_button_enter&fbclid=IwAR3aZT8GlXbvdywJEXbWGRVY-g1sl7GcKEs8kAAaWwiK3pNNxgM5Wdqgfm0
-var userinput = document.getElementById("searchbar");
-userinput.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("mag").click();
-    }
-});
-
 function search(userinput) {
     document.getElementById("images").innerHTML = "";
     var request = new XMLHttpRequest();
     var url = "https://www.rijksmuseum.nl/api/nl/collection?q=" + userinput + "&key=fMLJ55Eu&format=json"
     console.log(userinput);
     console.log(url);
-}
+    request.open('GET', url, true); // open a new connection, using the GET request on the URL endpoint
+    request.onload = function () { // begin accessing JSON data here
+        var imgdiv = document.getElementById("images");
+        var data = JSON.parse(this.response);
+        //console.log(data);
+        //console.log(data["artObjects"]);
+        //    debugger;
+        for (index in data["artObjects"]){
+            console.log(index);
+            console.log(data["artObjects"][index]["webImage"]["url"]);
+            var imgurl = data["artObjects"][index]["webImage"]["url"];
+                var img = document.createElement("img");
+                img.src = imgurl;
+                imgdiv.appendChild(img);
+            }
+        }
+    request.send();
+};
 
-// function myFirstFunction(input) {
-//     // location.href = "results.html";
-//     // this also works: window.open("https://www.youraddress.com","_self")
-//     // Create a request variable and assign a new XMLHttpRequest object to it.
-//     document.getElementById("pictures").innerHTML = "";
-//     var request = new XMLHttpRequest();
-//
-//     var query = "https://www.rijksmuseum.nl/api/nl/collection?q=" + input + "&key=fMLJ55Eu&format=json"
-//     console.log(query);
-//     // Open a new connection, using the GET request on the URL endpoint
-//     // request.open('GET', 'https://www.rijksmuseum.nl/api/en/collection/SK-C-5?key=fMLJ55Eu&format=json', true);
-//
-//     // request.open('GET', 'https://www.rijksmuseum.nl/api/nl/collection?key=fMLJ55Eu&format=json&type=schilderij&f.normalized32Colors.hex=%20%23367614', true);
-//     request.open('GET', query, true);
-//
-//     request.onload = function () {
-//       // Begin accessing JSON data here
-//       // Begin accessing JSON data here
-//         var imgdiv = document.getElementById("pictures");
-//         var data = JSON.parse(this.response);
-//         //console.log(data);
-//         //console.log(data["artObjects"]);
-//             debugger;
-//         for (index in data["artObjects"]){
-//             //console.log(index);
-//             console.log(data["artObjects"][index]["webImage"]["url"]);
-//             var imgurl = data["artObjects"][index]["webImage"]["url"];
-//                 var img = document.createElement("img");
-//                 img.src = imgurl;
-//                 imgdiv.appendChild(img);
-//             }
-//
-//         }
-//
-//     // Send request
-//     request.send();
-// };
+
+
+
+
+
+
+
 
 
 // inspired by: https://www.w3schools.com/howto/howto_js_collapsible.asp
@@ -87,3 +65,16 @@ function expandOrCollapsePanel3() {
         x.style.display = "block";
     }
 };
+
+
+
+
+// enter triggers a click on the magnifying glass
+// credits to https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_trigger_button_enter&fbclid=IwAR3aZT8GlXbvdywJEXbWGRVY-g1sl7GcKEs8kAAaWwiK3pNNxgM5Wdqgfm0
+var userinput = document.getElementById("searchbar");
+userinput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("mag").click();
+    }
+});
