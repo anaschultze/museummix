c8725d8ce5cbc69769c298ab009abf9c8bc1be1d
anaschultze <acmschultze@gmail.com>
2019-01-21 10:16:30 +0100

    grid for portrait function

--- portraitfunction.js
+++ portraitfunction.js
@@ -7,7 +7,7 @@ function portrait() {
 
     // Step 3: Call the Rijksmuseum API and add results to array
     var request = new XMLHttpRequest();
-    var url = "https://www.rijksmuseum.nl/api/nl/collection?q=" + "portrait" + "&key=fMLJ55Eu&format=json";
+    var url = "https://www.rijksmuseum.nl/api/en/collection?q=" + "portrait" + "&key=fMLJ55Eu&format=json";
     request.open('GET', url, false); // open a new connection, using the GET request on the URL endpoint
     request.onload = function () { // begin accessing JSON data here
 
@@ -71,15 +71,32 @@ function portrait() {
     if (plength != 0) {
         for (i = 0; i < plength; i++){
             console.log(i);
+
             var web = document.createElement("a");
             web.href = pictures[i]["weburl"];
+            web.className = "resultlink";
             web.target = "_blank";
-            web.class = "loadingpictures";
-            //web.text = pictures[i]["title"];
 
-            var pic = web.appendChild(document.createElement("img"));
+            var container = document.createElement("div");
+            container.className = "imgcontainer";
+
+            var pic = document.createElement("img");
             pic.src = pictures[i]["imgurl"];
+            pic.className = "image";
             pic.target = "_blank";
+            container.appendChild(pic);
+
+            var overlay = document.createElement("div");
+            overlay.className = "overlay";
+
+            var info = document.createElement("div");
+            info.textContent = pictures[i]["title"];
+            info.className = "text";
+            overlay.appendChild(info);
+
+            container.appendChild(overlay);
+
+            web.appendChild(container);
 
             imgdiv.appendChild(web);
         }
